381d86f2b03ade4b368c294effbba9eb135db3af
marko1rose <marko1rose@gmail.com>
2023-05-12 07:48:38 +0200

    Enabled caching

--- movie-service/src/main/java/com/demo/project/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/demo/project/movieservice/MovieServiceApplication.java
@@ -3,11 +3,13 @@ package com.demo.project.movieservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.demo.project.common.repository")
 @EntityScan("com.demo.project.common.model")
+@EnableCaching
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
